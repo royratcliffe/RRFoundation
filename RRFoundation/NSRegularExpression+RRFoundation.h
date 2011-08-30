5ce4648bbb398ed4cd513c3019d22f45c93b0cab
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2011-08-30 10:04:23 +0100

    Added example for -replaceMatchesInString:replacementStringForResult:

--- RRFoundation/NSRegularExpression+RRFoundation.h
+++ RRFoundation/NSRegularExpression+RRFoundation.h
@@ -31,6 +31,47 @@
  * given block to compute the replacement string for each match. The block
  * accepts the match result, the progressively replaced string along with its
  * progressive offset.
+ *
+ * Use the method as follows. Suppose you want to have strings of the form
+ * "a/b/c" and you want to replace every path element separator (forward slash)
+ * to double-colons and fold the following character to uppercase at the same
+ * time. This assumes the character following the slashes are letters of course,
+ * but let us take that as a given. We could you the regular expression "/(.?)"
+ * meaning a slash optionally followed by any character. The parentheses act as
+ * markers for substitution.
+ *
+ *	NSRegularExpression *re = [NSRegularExpression regularExpressionWithPattern:@"/(.?)"
+ *																		options:0
+ *																		  error:NULL];
+ *	NSString *answer = [re replaceMatchesInString:@"a/b/c"
+ *					   replacementStringForResult:^NSString *(NSTextCheckingResult *result,
+ *															  NSString *inString,
+ *															  NSInteger offset) {
+ *						   // Use “result” to access the regular expression
+ *						   // again. This step could be omitted because (in this
+ *						   // example) this interior block has access to the
+ *						   // exterior context, the “re” above. However, that is
+ *						   // not always the case. Accessing the original
+ *						   // regular expression via the matching result proves
+ *						   // the best general approach.
+ *						   NSRegularExpression *re = [result regularExpression];
+ *						   // Then pull out “s1” from the input string, the
+ *						   // string matching the first parentheses. The "$1"
+ *						   // template refers to this first matching part of the
+ *						   // entire regular expression match. This block runs
+ *						   // for every match. Of course, each match can have
+ *						   // multiple matching subsections. See Apple
+ *						   // documentation for NSRegularExpression for more
+ *						   // details.
+ *						   NSString *s1 = [re replacementStringForResult:result
+ *																inString:inString
+ *																  offset:offset
+ *																template:@"$1"];
+ *						   return [@"::" stringByAppendingString:[s1 uppercaseString]];
+ *					   }];
+ *	NSLog(@"\"%@\"", answer);
+ *
+ * The answer "a::B::C" appears in the log.
  */
 - (NSString *)replaceMatchesInString:(NSString *)string replacementStringForResult:(NSString *(^)(NSTextCheckingResult *result, NSString *inString, NSInteger offset))replacementStringForResult;
 
