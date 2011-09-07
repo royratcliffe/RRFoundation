9c09dc69a31c8cf0cbfb376336a0531ea6e4b270
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2011-09-07 10:12:18 +0100

    Reorganised comment block for -replaceMatchesInString:replacementStringForResult:

--- RRFoundation/NSRegularExpression+RRFoundation.h
+++ RRFoundation/NSRegularExpression+RRFoundation.h
@@ -33,12 +33,12 @@
  * progressive offset.
  *
  * Use the method as follows. Suppose you want to have strings of the form
- * "a/b/c" and you want to replace every path element separator (forward slash)
- * to double-colons and fold the following character to uppercase at the same
- * time. This assumes the character following the slashes are letters of course,
- * but let us take that as a given. We could you the regular expression "/(.?)"
- * meaning a slash optionally followed by any character. The parentheses act as
- * markers for substitution.
+ * "a/b/c" and you want to replace every path element separator (the forward
+ * slashes) to double-colons and fold the following character to uppercase at
+ * the same time. This assumes the characters following the slashes are
+ * letters. We could use the regular expression "/(.?)" meaning a slash
+ * optionally followed by any character. The parentheses act as markers for
+ * substitution.
  *
  *	NSRegularExpression *re = [NSRegularExpression regularExpressionWithPattern:@"/(.?)"
  *																		options:0
@@ -47,22 +47,9 @@
  *					   replacementStringForResult:^NSString *(NSTextCheckingResult *result,
  *															  NSString *inString,
  *															  NSInteger offset) {
- *						   // Use “result” to access the regular expression
- *						   // again. This step could be omitted because (in this
- *						   // example) this interior block has access to the
- *						   // exterior context, the “re” above. However, that is
- *						   // not always the case. Accessing the original
- *						   // regular expression via the matching result proves
- *						   // the best general approach.
+ *						   // See Note 1
  *						   NSRegularExpression *re = [result regularExpression];
- *						   // Then pull out “s1” from the input string, the
- *						   // string matching the first parentheses. The "$1"
- *						   // template refers to this first matching part of the
- *						   // entire regular expression match. This block runs
- *						   // for every match. Of course, each match can have
- *						   // multiple matching subsections. See Apple
- *						   // documentation for NSRegularExpression for more
- *						   // details.
+ *						   // See Note 2
  *						   NSString *s1 = [re replacementStringForResult:result
  *																inString:inString
  *																  offset:offset
@@ -72,6 +59,18 @@
  *	NSLog(@"\"%@\"", answer);
  *
  * The answer "a::B::C" appears in the log.
+ *
+ * Note 1: Uses “result” to access the regular expression again. This step could
+ * be omitted because (in this example) this interior block has access to the
+ * exterior context, the “re” above. However, that is not always the
+ * case. Accessing the original regular expression via the matching result
+ * proves the best general approach.
+ *
+ * Note 2: Pulls out “s1” from the input string, the string matching the first
+ * parentheses. The "$1" template refers to this first matching part of the
+ * entire regular expression match. The block runs for every match. Of course,
+ * each match can have multiple matching subsections. See Apple documentation
+ * for NSRegularExpression for more details.
  */
 - (NSString *)replaceMatchesInString:(NSString *)string replacementStringForResult:(NSString *(^)(NSTextCheckingResult *result, NSString *inString, NSInteger offset))replacementStringForResult;
 
