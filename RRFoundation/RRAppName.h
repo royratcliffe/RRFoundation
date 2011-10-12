3dc282ea5b3ad0fc46e3866b0ba8537529dbd6d6
Roy Ratcliffe <roy@pioneeringsoftware.co.uk>
2011-10-12 09:57:15 +0100

    Added localisation strings

--- RRFoundation/RRAppName.h
+++ RRFoundation/RRAppName.h
@@ -1,6 +1,6 @@
 // RRFoundation RRAppName.h
 //
-// Copyright © 2008, 2011, Roy Ratcliffe, Pioneering Software, United Kingdom
+// Copyright © 2008-2011, Roy Ratcliffe, Pioneering Software, United Kingdom
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the “Software”), to deal
